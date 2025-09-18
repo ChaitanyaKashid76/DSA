2e55239d246491edf434f4ecafb43e4f21bc49dd
Chaitanya Kashid <chaitanyakashid961@gmail.com>
2025-09-18 10:12:17 +0530

    Another way to declear an array and print  using for each loop

--- Array2.java
+++ Array2.java
@@ -1,4 +1,16 @@
 public class Array2 {
-    //another way to print an arrray
-    
-}
+    //not fix size array
+    public static void main(String [] args){
+    int[] arr={1,2,3,4};
+    for (int i=0;i<arr.length;i++)
+    {
+        System.out.println(i);
+    }
+    //using for each loop
+    for(int i:arr){
+        System.out.println(i);
+    }
+
+
+
+}}
